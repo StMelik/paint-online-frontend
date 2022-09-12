50de9321e6309cdeec9cdbed3ee401251b9365a4
Stanislav Meleshchik <st.mele.a@yandex.ru>
2022-09-12 03:03:37 +0300

    feat: добавить ластик

--- src/components/ToolBar.tsx
+++ src/components/ToolBar.tsx
@@ -3,6 +3,7 @@ import toolState from '../store/toolState';
 import '../styles/toolbar.scss'
 import Brush from '../tools/Brush';
 import Circle from '../tools/Circle';
+import Eraser from '../tools/Eraser';
 import Rect from '../tools/Rect';
 
 const ToolBar = () => {
@@ -23,6 +24,7 @@ const ToolBar = () => {
             />
             <button
                 className='toolbar__button toolbar__button_eraser'
+                onClick={() => toolState.setTool(new Eraser(canvasState.canvas))}
             />
             <button
                 className='toolbar__button toolbar__button_line'
---
+++ src/tools/Eraser.ts
@@ -0,0 +1,39 @@
+import Tool from "./Tool";
+
+export default class Eraser extends Tool {
+    mouseDown: boolean;
+
+    constructor(canvas: HTMLCanvasElement) {
+        super(canvas)
+        this.listen()
+        this.mouseDown = false
+    }
+
+    listen(): void {
+        this.canvas.onmouseup = this.mouseUpHandler.bind(this)
+        this.canvas.onmousedown = this.mouseDownHandler.bind(this)
+        this.canvas.onmousemove = this.mouseMoveHandler.bind(this)
+    }
+
+    mouseUpHandler(e: MouseEvent): void {
+        this.mouseDown = false
+    }
+
+    mouseDownHandler(e: MouseEvent): void {
+        this.mouseDown = true
+        this.ctx.beginPath()
+        this.ctx.moveTo(e.offsetX, e.offsetY)
+    }
+
+    mouseMoveHandler(e: MouseEvent): void {
+        if (this.mouseDown) {
+            this.draw(e.offsetX, e.offsetY)
+        }
+    }
+
+    draw(x: number, y: number): void {
+        this.ctx.lineTo(x, y)
+        this.ctx.strokeStyle = "#ffffff"
+        this.ctx.stroke()
+    }
+}
